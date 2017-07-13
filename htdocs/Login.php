e92819efe0246b7a1aacaac1a046ebcc2ede6087
cit-ishikawa <s1542014bb@s.chibakoudai.jp>
2017-07-13 17:46:26 +0900

    Add files via upload

---
+++ htdocs/Login.php
@@ -0,0 +1,38 @@
+<DOCTYPE html>
+<html>
+<head>
+<meta charest="UTF-8">
+<link rel="stylesheet"href="Staff.css">
+</head>
+<body>
+<div>
+売買マッチングシステム<br/>
+<br/>
+ログイン
+<table>
+<form method="post"action="Login_check.php">
+<tr>
+<td>
+</br>
+</td>
+</tr>
+<tr>
+ <td>ID<br/></td>
+ <td><input type="text"name="name"maxlength="8"pattern="^[0-9A-Za-z]+$|[\uFF21-\uFF3A｜\uFF41-\uFF5A]*|[\uFF10-\uFF19]*"></td>
+</tr>
+<tr>
+ <td>パスワード<br/></td>
+ <td><input type="password"name="pass"maxlength="8"pattern="^[0-9A-Za-z]+$|[\uFF21-\uFF3A｜\uFF41-\uFF5A]*|[\uFF10-\uFF19]*"></td>
+   <tr>
+  <td>
+  <br/>
+  </td>
+  </tr>
+  <tr>
+</table>
+<a href="Touroku.php">新規登録</a>
+<input type="submit"value="ログイン">
+</div>
+
+</body>
+</html>
